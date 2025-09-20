a7b879ef7cc645710d3e10c1eabfff078e8449af
Stjepan Đelekovčan <45045855+jetpans@users.noreply.github.com>
2025-09-20 16:22:07 +0200

    feat: changed service2

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -7,4 +7,5 @@ SERVICE1_URL = os.getenv("SERVICE1_URL", "http://err:8080")
 message = requests.get(sys.stdin.readline()).text
 data = ["md5", message]
 
+
 print(requests.post(SERVICE1_URL, data="\n".join(data)).text)
